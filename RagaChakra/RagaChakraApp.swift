f05f0c41830cbac3e019cbee1ab817e1239d35de
Venky Venkatakrishnan <eeshwar1@gmail.com>
2023-09-22 22:35:04 -0500

    Initial Commit

---
+++ RagaChakra/RagaChakraApp.swift
@@ -0,0 +1,17 @@
+//
+//  RagaChakraApp.swift
+//  RagaChakra
+//
+//  Created by Venkateswaran Venkatakrishnan on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct RagaChakraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
